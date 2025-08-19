e61228bdadd0843f8222a108851b8ef11bf4a7e9
prafful9111 <100062223+prafful9111@users.noreply.github.com>
2025-08-19 18:32:29 +0530

    Fix errors

--- app/roles/rider/_layout.tsx
+++ app/roles/rider/_layout.tsx
@@ -9,19 +9,15 @@ export default function RiderLayout() {
       }}
     >
       {/* Map file routes to drawer items with nice titles */}
-      <Drawer.Screen name="rideroverview" options={{ title: "Rider Overview" }} />
+      <Drawer.Screen name="RiderOverview" options={{ title: "Rider Overview" }} />
 
-      <Drawer.Screen name="logsales" options={{ title: "Log Sales" }} />
+      <Drawer.Screen name="RiderLogs" options={{ title: "Log Sales" }} />
 
-      <Drawer.Screen name="carthealth" options={{ title: "Cart Health" }} />  
-      <Drawer.Screen name="myroute" options={{ title: "My Route" }} />
-      <Drawer.Screen name="requestmore" options={{ title: "Request More" }} />
-      <Drawer.Screen name ="avialablevehicles" options={{title:"Available Vehicles"}}/>
-      <Drawer.Screen name="myinventory" options={{ title: "My Inventory" }} />
-
-
-
-
+      <Drawer.Screen name="CartHealth" options={{ title: "Cart Health" }} />  
+      <Drawer.Screen name="MyRoute" options={{ title: "My Route" }} />
+      <Drawer.Screen name="RequestMore" options={{ title: "Request More" }} />
+      <Drawer.Screen name ="AvialableVehicles" options={{title:"Available Vehicles"}}/> 
+      <Drawer.Screen name="MyInventory" options={{ title: "My Inventory" }} />
 
     </Drawer>
   );
--- app/roles/supervisor/_layout.tsx
+++ app/roles/supervisor/_layout.tsx
@@ -9,14 +9,11 @@ export default function SupervisorLayout() {
       }}
     >
       {/* Map file routes to drawer items with nice titles */}
-      <Drawer.Screen name="supervisoroverview" options={{ title: "Supervisor Overview" }} />
-      <Drawer.Screen name="riderlogs" options={{ title: "Rider Logs" }} />
-      <Drawer.Screen name="assignrider" options={{ title: "Assign Rider" }} />
-      <Drawer.Screen name="viewinvantory" options={{ title: "View Invantory" }} />
-      <Drawer.Screen name="viewroutes" options={{ title: "View Routes" }} />
-
-
-
+      <Drawer.Screen name="SupervisorOverview" options={{ title: "Supervisor Overview" }} />
+      <Drawer.Screen name="RiderLogs" options={{ title: "Rider Logs" }} />
+      <Drawer.Screen name="AssignRider" options={{ title: "Assign Rider" }} />
+      <Drawer.Screen name="ViewInventory" options={{ title: "View Invantory" }} />
+      <Drawer.Screen name="ViewRoutes" options={{ title: "View Routes" }} />
 
     </Drawer>
   );
